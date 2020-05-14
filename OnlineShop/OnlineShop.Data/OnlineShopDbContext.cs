dd33909421e6d95f59465ed4d6d7b1908a26c623
Leo Deg <rdkdvv@gmail.com>
2020-05-14 22:42:45 +0300

    Initialize project

---
+++ OnlineShop/OnlineShop.Data/OnlineShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShop.Data
+{
+	public class OnlineShopDbContext : DbContext
+	{
+		public OnlineShopDbContext(DbContextOptions<OnlineShopDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
